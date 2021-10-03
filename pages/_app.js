59a5a6e6726ec287c5243a335807214dadb682ce
Indrajith V <57178742+indrajithvgp@users.noreply.github.com>
2021-10-03 11:57:13 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+      <Head>
+        <title>Eth-KickStarter</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
